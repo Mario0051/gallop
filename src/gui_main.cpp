a44c442431288b4c27fc8ff78cec7c5842eebd53
Mario <windowsmario64@gmail.com>
2025-11-28 01:28:37 -0800

    Update CMakeLists.txt
    Update gui_main.cpp

--- src/gui_main.cpp
+++ src/gui_main.cpp
@@ -2,14 +2,19 @@
 #include <thread>
 #include <vector>
 #include <string>
+#include <filesystem>
 #include "imgui.h"
 #include "imgui_sink.hpp"
 #include "spdlog/spdlog.h"
+#include "config.hpp"
+#include "mdb.hpp"
 
 namespace gallop {
     extern bool done;
 
     std::shared_ptr<gui::imgui_sink_mt> sink;
+
+    std::filesystem::path path = std::filesystem::current_path();
 }
 
 namespace gallop::gui {
@@ -55,10 +60,13 @@ extern "C" int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     gallop::sink = std::make_shared<gallop::gui::imgui_sink_mt>();
     auto logger = std::make_shared<spdlog::logger>("gui_logger", gallop::sink);
     spdlog::set_default_logger(logger);
-    spdlog::set_pattern("[%H:%M:%S] %v");
+    spdlog::set_pattern("[%l] %v");
 
     std::thread pipeThread(PipeServerThread);
     pipeThread.detach();
+
+    gallop::init_config();
+    gallop::init_mdb();
 
     if (gallop::gui::init() != 0) return 1;
 
